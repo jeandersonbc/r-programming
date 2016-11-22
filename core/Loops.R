9adb3812787c1304806f27a41155551810a2d28c
Jeanderson Candido <jeandersonbc@gmail.com>
2016-11-22 14:06:29 -0300

    added for-loop

--- core/Loops.R
+++ core/Loops.R
@@ -1,6 +1,11 @@
 # while-loop just like any other c-like prog. languange
 i <- 0
-while(i < 10) {
+while (i < 10) {
   print(i) # within a loop, we need to use the function print
   i <- i + 1
+}
+
+# for-loops in R are like for-each loops.
+for (i in 1:10) {
+  print(i)
 }
